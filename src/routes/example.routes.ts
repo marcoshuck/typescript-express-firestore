01456a1a7661f774da20c7600303ce2567d4eea1
Marcos Huck <marcos.h3012@gmail.com>
2019-01-03 18:19:00 -0300

    ADD Example routes

---
+++ src/routes/example.routes.ts
@@ -0,0 +1,12 @@
+import {Router} from 'express';
+import ExampleController from '../controllers/example.controller';
+
+const router: Router = Router();
+
+router.post('/', ExampleController.create);
+router.get('/:id', ExampleController.readOne);
+router.get('/', ExampleController.readAll);
+router.put('/:id', ExampleController.update);
+router.delete('/:id', ExampleController.delete);
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import {Router} from 'express';
+
+import example from './example.routes';
+
 const router: Router = Router();
 
-// router.use('/endpoint', example);
+router.use('/examples', example);
 
 export default router;

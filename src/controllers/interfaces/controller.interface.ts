e46e9922b97ec92d828930bcabd0583a30068f3e
Marcos Huck <marcos.h3012@gmail.com>
2019-01-04 04:01:44 -0300

    CHANGE controller interface due to an error un private property

--- src/controllers/interfaces/controller.interface.ts
+++ src/controllers/interfaces/controller.interface.ts
@@ -1,9 +1,7 @@
 import {Request, Response} from 'express';
 import { DocumentData } from '@google-cloud/firestore';
-import FirestoreRepository from '../../repositories/firestore.repository';
 
 export default interface IController {
-    repository: FirestoreRepository;
     create(req: Request, res: Response): DocumentData | undefined;
     readOne(req: Request, res: Response): DocumentData | undefined;
     readAll(req: Request, res: Response): DocumentData[] | undefined;
--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,7 @@ const key = require('../keys/serviceAccountKey.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(key),
-    databaseURL: "https://example.firebaseio.com"
+    databaseURL: "https://fencing-tournament.firebaseio.com"
 });
 
 const db: FirebaseFirestore.Firestore = admin.firestore();

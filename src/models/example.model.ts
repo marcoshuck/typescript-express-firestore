33700b9bfb5275b3b38dc6ef77e1a7a74caa9fc6
Marcos Huck <marcos.h3012@gmail.com>
2019-01-08 19:54:11 -0300

    Add more decorators to model

--- src/models/example.model.ts
+++ src/models/example.model.ts
@@ -1,36 +1,44 @@
 import IExample from "./interfaces/example.interface";
 import { Timestamp } from "@google-cloud/firestore";
-import {IsString, IsNotEmpty} from "class-validator";
+import {IsString, IsNotEmpty, IsAlpha} from "class-validator";
 
 export default class Example implements IExample {
   @IsString()
+  @IsAlpha()
   @IsNotEmpty()
   public firstName?: string;
 
   @IsString()
+  @IsAlpha()
   @IsNotEmpty()
   public middleName?: string;
 
   @IsString()
+  @IsAlpha()
   @IsNotEmpty()
   public lastName?: string;
+
 
   @IsNotEmpty()
   public birthday?: Timestamp;
 
   @IsString()
+  @IsAlpha()
   @IsNotEmpty()
   public country?: string;
 
   @IsString()
+  @IsAlpha()
   @IsNotEmpty()
   public province?: string;
 
   @IsString()
+  @IsAlpha()
   @IsNotEmpty()
   public city?: string;
 
   @IsString()
+  @IsAlpha()
   @IsNotEmpty()
   public genre?: string;
 }
